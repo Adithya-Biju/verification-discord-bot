d98222654c20184f16105d48d84153938bf91a0e
adithya <adithyabiju@gmail.com>
2026-02-06 07:31:13 +0530

    DB hiccup

--- cogs/role_update_queue.py
+++ cogs/role_update_queue.py
@@ -2,7 +2,8 @@ from discord.ext import tasks
 from models import RoleUpdateQueue, Customer
 from utility.server_roles import role_updates 
 from discord.ext import commands
-
+from asyncpg.exceptions import ConnectionDoesNotExistError
+from tortoise.exceptions import DBConnectionError
 
 class RoleUpdates(commands.Cog):
     def __init__(self, bot):
@@ -15,17 +16,23 @@ class RoleUpdates(commands.Cog):
 
     @tasks.loop(seconds=5) 
     async def process_role_updates(self):
+
+        try:
         
-        pending_update = await RoleUpdateQueue.all().order_by('id').first()
+            pending_update = await RoleUpdateQueue.all().order_by('id').first()
 
-        if pending_update:
-            user_id = pending_update.user_id
+            if pending_update:
+                user_id = pending_update.user_id
 
-            await role_updates(self.bot, user_id)
-            await pending_update.delete()
+                await role_updates(self.bot, user_id)
+                await pending_update.delete()
 
-        else:
-            return 
+            else:
+                return 
+        except (ConnectionDoesNotExistError, DBConnectionError) as e:
+            print(f"Database connection issue: {e}. Retrying in 5 seconds...")
+        except Exception as e:
+            print(f"Unexpected error in role update loop: {e}")
 
 async def setup(bot):
     await bot.add_cog(RoleUpdates(bot))
